91dcd73f857cbf78c4e807c7ac93f16f8e41d822
Nguyen Phan Thao Nguyen <thaonguyen68161@gmail.com>
2024-11-14 16:33:18 +0700

    edit nav bar

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,15 +1,15 @@
 import React, {useState} from "react";
 import {Form, Button} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
-const Gift = () => {
+const Cart = () => {
     
     return (
         <>
             <div className="center-form">
-                <h1>Gift page</h1>
+                <h1>Cart page</h1>
             </div>
         </>
     );
 };
 
-export default Gift;
+export default Cart;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import HomeHeader from "../components/header/homeHeader";
-import Gift from "../components/gift/gift";
+import Cart from "../components/cart/cart";
 import Task from "../components/task/task";
 import Home from "../components/home/home";
 import Profile from "../components/profile/profile";
@@ -12,7 +12,7 @@ function HomePage() {
       <HomeHeader></HomeHeader>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/gift" element={<Gift></Gift>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
         <Route path="/task" element={<Task></Task>}></Route>
         <Route path="/profile" element={<Profile></Profile>}></Route>
       </Routes>
